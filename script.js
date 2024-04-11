59562c47b5c024fd0fd433cac19803383de857c3
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-04-11 17:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let output='';
+for(let i=0;i<=20;i++){
+	if(i%3==0 || i%5==0){
+		output=output+"FizzBuzz\n";
+	}
+	else if(i%3==0){
+		output=output+"Fizz\n";
+	}
+	else if(i%5==0){
+		output=output+"Buzz\n";
+	}
+	else(
+		output=output+i+"\n";
+	)
+}
+alert(output);
